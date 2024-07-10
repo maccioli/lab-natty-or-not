cdb4c2fbd26d5d3a95bacb15b8c2c699c4061349
Marcio Accioli <mjaccioli@gmail.com>
2024-07-10 17:01:58 -0300

    Funcao pegar_parametros

--- genimage.py
+++ genimage.py
@@ -5,3 +5,11 @@ def pegar_parameteros():
     personagem_descricao = ""
     if tem_personagem == "sim":
         personagem_descricao = input("Descreva o personagem: ").strip()
+
+    tem_cenario = input("A imagem tem cenário? (sim/não): ").strip().lower()
+    cenario_descricao = ""
+    if tem_cenario == "sim":
+        cenario_descricao = input("Descreva o cenário: ").strip()
+
+    return personagem_descricao, cenario_descricao
+
